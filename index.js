705a6f366a03b428c67055e94cae535c3895518f
moseshub <62851621+moseshub@users.noreply.github.com>
2020-03-29 21:25:29 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function convertFtoI() {
+        var feetInput = document.getElementsByName('feetUnit')
+        var feetToInchesResult = feetInput * 12
+        alert(`${feetInput} feet is equal to ${feetToInchesResult} inches!`);
+    }
+
+    function convertItoF() {
+        var inchesInput = document.getElementsByName('inchesUnit')
+        var inchesToFeetResult = inchesInput * 12
+        alert(`${inchesInput} feet is equal to ${inchesToFeetResult} feet!`);
+    }    
